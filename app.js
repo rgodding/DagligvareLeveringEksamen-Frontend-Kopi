f09efe568ea5b2193633eeda01315da194addaa6
Shaundator <90447626+Shaundator@users.noreply.github.com>
2022-12-23 12:49:33 +0100

    layout for product created

---
+++ app.js
@@ -0,0 +1,15 @@
+function testValue(){
+    console.log('test value')
+    document.getElementById('add-product-name-id').value = '123'
+}
+
+
+document.getElementById('patch-product-form').addEventListener("click", function(event){
+    event.preventDefault()
+})
+function registerClick(e) {
+  e.preventDefault();
+}
+function registerSubmit(e) {
+  e.preventDefault();
+}
